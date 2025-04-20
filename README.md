0e50d8915ee6903bdf216bd97b1ee7f51d89b71d
Vasily Kulikov <segoon@yandex-team.ru>
2025-04-20 18:16:23 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Quickstart
+
+## Installation
+
+Write the following into your `init.lua`:
+
+```lua
+Plug('segoon/goto-arcanum')
+```
+
+## Usage
+
+```
+:OpenInArcanum
+```
+
+Or use nvim_set_keymap() to setup keymap.
---
+++ plugin/goto-arcanum/init.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+local function open_url(url)
+  vim.system({'xdg-open', url}):wait()
+  print(url)
+end
+
+local function open_in_arcanum(opts)
+    local fname = vim.api.nvim_buf_get_name(0)
+    local dirname = vim.fs.dirname(fname)
+    local arc_root = vim.system({'arc', 'root'}, {cwd = dirname}):wait().stdout
+    if not arc_root then
+      print('arc root returned empty, not in arcadia?')
+      return nil
+    end
+
+    local relpath = string.sub(fname, arc_root:len() + 1)
+    relpath = relpath:gsub('%s+', '')
+
+    local linenr = vim.api.nvim_win_get_cursor(0)[1]
+
+    open_url('https://a.yandex-team.ru/arcadia/' .. relpath .. '#L' .. linenr)
+end
+
+vim.api.nvim_create_user_command(
+  "OpenInArcanum",
+  open_in_arcanum,
+  {}
+)
+
+return M
